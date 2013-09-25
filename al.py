ef079200d0cbb147e0b95f3e333b34156821832c
Christopher Yarger <cpyarger@Phoenix.(none)>
2013-09-25 11:57:33 -0400

    added al.py

---
+++ al.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# 
+# Refrenced https://github.com/warlockcc/snippets/blob/master/favorite-artists/favorite-artists.py
+# in the design of this script. 
+#
+# Modifications were needed to have this script produce the requested output.	`
+
+
+from collections import defaultdict
+from itertools import combinations
+
+
+stats = defaultdict(int)
+
+with open('Artist_lists_small.txt', 'r') as f: # opens File for reading
+  for line in f:# reads file line by line
+    for pair in combinations(line.split(','), 2): #Finds Pairs
+      stats[pair] += 1 # counts pairs
+
+# Removes Self Pairs
+stats = [(k, v) for k, v in stats.items() if k[0] != k[1]]
+
+#Prints only pairs that show up in list 50 times or higher
+
+for p in sorted(stats, key=lambda x: x[1], reverse=True): 
+   if ( p[1] >= 50 ): #parses for stats equal or greater than 50
+    print(p) # prints out those pairs that appear in 50 or more lists
+
+
+
